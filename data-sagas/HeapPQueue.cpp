56fcd986c868603547b45faf4b0d67060bb91306
Paul <lecomtepaulvd@gmail.com>
2025-08-17 09:47:58 +0200

    preparing new assignements

---
+++ data-sagas/HeapPQueue.cpp
@@ -0,0 +1,26 @@
+#include "HeapPQueue.h"
+
+HeapPQueue::HeapPQueue() {}
+HeapPQueue::~HeapPQueue() {}
+
+void HeapPQueue::enqueue(const DataPoint& value) {
+    // TODO
+}
+
+DataPoint HeapPQueue::dequeue() {
+    // TODO
+    return {"", -1};
+}
+
+bool HeapPQueue::isEmpty() const {
+    // TODO
+    return true;
+}
+
+void HeapPQueue::bubbleUp(int index) {
+    // TODO
+}
+
+void HeapPQueue::bubbleDown(int index) {
+    // TODO
+}
