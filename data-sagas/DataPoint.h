e65f4e94c2ce321952b95b720b268bfa844d025b
Paul <lecomtepaulvd@gmail.com>
2025-08-18 08:00:53 +0200

    preparing new assignements

--- data-sagas/DataPoint.h
+++ data-sagas/DataPoint.h
@@ -1,7 +1,15 @@
 #pragma once
 #include <string>
+#include <iostream>
 
 struct DataPoint {
     std::string name;
     int weight;
+
+    DataPoint(const std::string& n = "", int w = 0) : name(n), weight(w) {}
 };
+
+inline std::ostream& operator<<(std::ostream& out, const DataPoint& dp) {
+    out << "(" << dp.name << ", " << dp.weight << ")";
+    return out;
+}
