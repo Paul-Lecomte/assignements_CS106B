6e01ca3677bfa8c7cebe3f423ea9bf827e78a0ee
Paul <lecomtepaulvd@gmail.com>
2025-08-18 08:13:49 +0200

    working on it

--- data-sagas/ExploreArrays.cpp
+++ data-sagas/ExploreArrays.cpp
@@ -4,6 +4,11 @@ void exploreArrays() {
     std::cout << "Running array experiments...\n";
     // TODO: implement Part 1 experiments
 	// Run experiments on arrays to understand performance and behavior
+	std::cout << "Running arrat experiments... \n";
+
+	// Example: Create an array of integers and perform some operations
+	int n = 5;
+
 
 
 }
