a32b9436f02b3209b18db1834f2965d5b151b668
Paul <lecomtepaulvd@gmail.com>
2025-08-18 08:35:26 +0200

    working on it

--- data-sagas/ExploreArrays.cpp
+++ data-sagas/ExploreArrays.cpp
@@ -5,7 +5,7 @@ void exploreArrays() {
     std::cout << "Running array experiments...\n";
     // TODO: implement Part 1 experiments
 	// Run experiments on arrays to understand performance and behavior
-	std::cout << "Running arrat experiments... \n";
+	std::cout << "Running array experiments... \n";
 
 	// Example: Create an array of integers and perform some operations
 	int n = 5;
@@ -14,4 +14,6 @@ void exploreArrays() {
 	for (int i = 0; i < n; i++) {
 		arr[i] = i * i; // Fill the array with squares of indices
 	}
+
+	// TODO : add timing experiments for growth and resizing
 }
